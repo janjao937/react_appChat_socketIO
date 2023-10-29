1a2d82d3a4271f0eac6868e4c90a26dc2607b027
janjao937 <janjao937@gmail.com>
2023-10-30 02:45:01 +0700

    socketIO react  appChat

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import io from "socket.io-client";
+import {useState} from "react"
+import Chat from './Chat';
+const socket = io.connect("http://localhost:3001");
 
 function App() {
+
+  const [username,setUsername] = useState("");
+  const [room,setRoom] = useState("");
+  const [showChat,setShowChat] = useState(false);
+  
+  const joinRoom = ()=>{
+    if(username&&room !== ""){
+      // console.log(username);
+      // console.log(room);
+      socket.emit("join_room",room);//sent RoomId to back
+      setShowChat(!showChat);
+    }
+
+  }
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">{
+      !showChat?(
+        <>
+        <h1>App Chat</h1>
+        <div>
+        <h2>Join Room</h2>
+        <input type="text" placeholder="name..." onChange={(e)=>setUsername(e.target.value)}  value={username}/>
+        <input type="text" placeholder="Room id..." onChange={(e)=>setRoom(e.target.value)} value={room} />
+        <button onClick={joinRoom}>Join Room</button>
+        </div>
+        </>
+      ): <Chat socket={socket} username={username} room={room} />
+    }
+      
+       
+     
     </div>
   );
 }
---
+++ src/Chat.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+
+const Chat =({socket,username,room})=>{
+    const [inPutMessage,setInputMessage] = useState("");
+    const [messageArr,setMessageArr] = useState([]);
+
+    const OnSendMessageHandler = async()=>{
+        if(inPutMessage==="") return;
+        console.log(inPutMessage);
+        //msgobject data
+        const messageData = {
+            room:room,
+            author:username,
+            message:inPutMessage,
+            time:new Date(Date.now()).getHours()+":"+new Date(Date.now()).getMinutes(),
+        }
+
+        //send_message event name like back
+        await socket.emit("send_message",messageData);
+        setMessageArr([...messageArr,messageData]);
+        setInputMessage("");
+    }
+
+    useEffect(()=>{
+        socket.on("recive_message",(data)=>{
+            console.log(data);
+            setMessageArr((e)=>[...e,data]);
+        })
+    },[socket]);
+    return(
+        <div>
+            <div className="chat-header">
+                <p>Realtime Chat|ROOM_ID:{room}</p>
+            </div>
+            <div className="chat-body">
+                {
+                    messageArr.map((e,index)=>{
+                        return (username === e.author?<div  key={index} className="message">
+                            <div  style={{display:"flex",justifyContent:"right", gap:"4px", border:"1px solid", margin:"25px" }}>
+                               <p>{e.message}</p>
+                                <p style={{color:"green"}}>:{e.author}
+                                <sub style={{color:"brown"}}>{e.time}</sub>
+                                </p>
+                                
+                            </div>
+                            
+                        </div>:<div  key={index} className="message">
+                            <div  style={{display:"flex",justifyContent:"left", gap:"4px", border:"1px solid", margin:"25px" }}>
+                                <p style={{color:"red"}} ><sub style={{color:"brown"}}>{e.time}</sub> {e.author}:</p>
+                               <p>
+                               {e.message}</p>
+                                
+                            </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <div className="chat-footer">
+                <input type="text" placeholder="message..." onChange={(e)=>setInputMessage(e.target.value)} value={inPutMessage}/>
+                <button onClick={OnSendMessageHandler}>Send</button>
+            </div>
+        </div>
+    )
+}
+
+export default Chat;
