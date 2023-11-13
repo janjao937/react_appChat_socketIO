88fca006e8a8565b82192b8be4b6d269c6097a85
janjao937 <janjao937@gmail.com>
2023-11-13 23:55:14 +0700

    change port

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import io from "socket.io-client";
 import {useState} from "react"
 import Chat from './Chat';
+// http://localhost:3001
+// "http://localhost:8888"
 const socket = io.connect("http://localhost:3001");
 
 function App() {
